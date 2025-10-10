86c9d7d001edc929abdbb4ded0182b1b3ea128a6
Hudzaifah-Hanafie <hanafiyaahmad@gmail.com>
2025-10-10 09:54:01 +0700

    menambahkan penyelesaian problem two-sum

---
+++ 1-Two-Sum/script.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    for (let i = 0; i < nums.length; i++) {
+        for (let j = i + 1; j < nums.length; j++) {
+            if (nums[i] + nums[j] == target) {
+                return [i, j];
+            }
+        }
+    }
+};
+
+const number = [2,7,11,15];
+const target = 9;
+twoSum(number, target);
